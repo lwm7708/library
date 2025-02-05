6ed0ca75da3ff4746c22f20b28a6a3e0843a5150
Brian Law <lwm7708@gmail.com>
2025-02-04 21:09:12 -0800

    Update `extended_gcd`

--- math/number_theory/extended_gcd.cpp
+++ math/number_theory/extended_gcd.cpp
@@ -6,18 +6,18 @@
 template <typename T>
 std::array<T, 3> extended_gcd(T m, T n) {
 
-    T a = 1;
-    T a_in = 0;
-    T b = 0;
-    T b_in = 1;
+    T a_m = 1;
+    T a_n = 0;
+    T b_m = 0;
+    T b_n = 1;
 
     while (n) {
         const T q = m / n;
-        a_in = std::exchange(a, a_in) - a_in * q;
-        b_in = std::exchange(b, b_in) - b_in * q;
+        a_n = std::exchange(a_m, a_n) - a_n * q;
+        b_n = std::exchange(b_m, b_n) - b_n * q;
         n = std::exchange(m, n) - n * q;
     }
 
-    return std::array<T, 3>({m, a, b});
+    return std::array<T, 3>({m, a_m, b_m});
 
 }
