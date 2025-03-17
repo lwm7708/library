8b676c0d9b9812d69c86d9b7b2e616b8c158cf08
Brian Law <lwm7708@gmail.com>
2025-03-16 20:43:17 -0700

    Add `long_path_decomposition`

--- test.cpp
+++ test.cpp
@@ -22,6 +22,7 @@
 #include "graph/centroid_decomposition.cpp"
 #include "graph/dinic.cpp"
 #include "graph/heavy_light_decomposition.cpp"
+#include "graph/long_path_decomposition.cpp"
 #include "hashing/integral_hash.cpp"
 #include "math/algebra/diophantine.cpp"
 #include "math/algebra/fast_fourier_transform.cpp"
@@ -473,6 +474,7 @@ TEST_CASE("fenwick_tree") {
 TEST_CASE("heavy_light_decomposition") {
 
     using rng_t = std::array<std::int32_t, 2>;
+    using vec_t = std::vector<std::int32_t>;
 
     heavy_light_decomposition decomp(11);
 
@@ -488,6 +490,8 @@ TEST_CASE("heavy_light_decomposition") {
     decomp.add_edge(5, 10);
 
     decomp.init(0);
+
+    CHECK(decomp.dfn == vec_t({0, 1, 8, 2, 5, 3, 6, 7, 9, 10, 4}));
 
     std::queue<rng_t> rngs;
 
@@ -518,6 +522,8 @@ TEST_CASE("heavy_light_decomposition") {
 
     decomp.init(1);
 
+    CHECK(decomp.dfn == vec_t({3, 0, 2, 1}));
+
     rngs.push(rng_t({1, 2}));
 
     decomp.for_path(3, 3, pop);
@@ -543,6 +549,43 @@ TEST_CASE("kasai") {
     const std::string str("dcafac ");
 
     CHECK(kasai(std::begin(str), vec_t({6, 4, 2, 5, 1, 0, 3})) == vec_t({0, 1, 0, 1, 0, 0}));
+
+}
+
+TEST_CASE("long_path_decomposition") {
+
+    using vec_t = std::vector<std::int32_t>;
+
+    long_path_decomposition decomp(11);
+
+    decomp.add_edge(0, 1);
+    decomp.add_edge(0, 2);
+    decomp.add_edge(1, 3);
+    decomp.add_edge(1, 4);
+    decomp.add_edge(3, 5);
+    decomp.add_edge(4, 6);
+    decomp.add_edge(4, 7);
+    decomp.add_edge(2, 8);
+    decomp.add_edge(8, 9);
+    decomp.add_edge(5, 10);
+
+    decomp.init(0);
+
+    CHECK(decomp.dfn == vec_t({0, 1, 8, 2, 5, 3, 6, 7, 9, 10, 4}));
+    CHECK(decomp.chds == vec_t({1, 3, 8, 5, 6, 10, -1, -1, 9, -1, -1}));
+    CHECK(decomp.deps == vec_t({4, 3, 2, 2, 1, 1, 0, 0, 1, 0, 0}));
+
+    decomp = long_path_decomposition(4);
+
+    decomp.add_edge(2, 3);
+    decomp.add_edge(3, 1);
+    decomp.add_edge(0, 1);
+
+    decomp.init(1);
+
+    CHECK(decomp.dfn == vec_t({3, 0, 2, 1}));
+    CHECK(decomp.chds == vec_t({-1, 3, -1, 2}));
+    CHECK(decomp.deps == vec_t({0, 2, 0, 1}));
 
 }
 
